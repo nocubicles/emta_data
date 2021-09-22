c2d94669e2b256c4cce5898aac5fd8d66a7f335a
Villem Heinsalu <villem@itera.ee>
2021-09-22 22:49:55 +0300

    First commit

---
+++ Migrations/20210922190530_initial2.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace estonian_companies.Migrations
+{
+    public partial class initial2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "YearQuarter",
+                table: "customerData",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "YearQuarter",
+                table: "customerData");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+This project takes data from https://www.emta.ee/et/kontaktid-ja-ametist/avaandmed-maksulaekumine-statistika/tasutud-maksud-kaive-ja-tootajate-arv and formats them and saves to a database.
+
+Data can be found from data.csv file.
